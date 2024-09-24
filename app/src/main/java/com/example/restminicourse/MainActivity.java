bb0a39afa9a9ae37efa2ba0760e84589cfcb217b
AccidentalDev <serge_cfcu@hotmail.com>
2024-09-23 20:09:03 -0600

    + Added the rest of the code from the original source.

--- app/src/main/java/com/example/restminicourse/MainActivity.java
+++ app/src/main/java/com/example/restminicourse/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.restminicourse;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +11,19 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.restminicourse.pojo.MultipleResource;
+import com.example.restminicourse.pojo.User;
+import com.example.restminicourse.pojo.UserList;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class MainActivity extends AppCompatActivity {
+    TextView responseText;
+    APIInterface apiInterface;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +36,98 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        responseText = findViewById(R.id.responseText);
+        apiInterface = APIClient.getClient().create(APIInterface.class);
+
+        //GET list resources
+        Call<MultipleResource> call = apiInterface.doGetListResources();
+        call.enqueue(new Callback<MultipleResource>() {
+            @Override
+            public void onResponse(Call<MultipleResource> call, Response<MultipleResource> response) {
+                Log.d("RESTUT", response.code()+"");
+
+                String displayResponse = "";
+                MultipleResource resource = response.body();
+                Integer text = resource.page;
+                Integer total = resource.total;
+                Integer totalPages = resource.totalPages;
+                List<MultipleResource.Datum> datumList = resource.data;
+
+                displayResponse += text+" Page\n"+total+" Total\n"+totalPages+" Total Pages\n";
+                for(MultipleResource.Datum datum : datumList){
+                    displayResponse += datum.id+" "+datum.name+" "+datum.pantoneValue+" "+ datum.year+"\n";
+                }
+                responseText.setText(displayResponse);
+            }
+
+            @Override
+            public void onFailure(Call<MultipleResource> call, Throwable t) {
+                call.cancel();
+            }
+        });
+
+        //Create new user
+        User user = new User("Morpheus", "Leader");
+        Call<User> userCall = apiInterface.createUser(user);
+        userCall.enqueue(new Callback<User>() {
+            @Override
+            public void onResponse(Call<User> call, Response<User> response) {
+                User tmpUser = response.body();
+                Toast.makeText(getApplicationContext(), tmpUser.name+" "+tmpUser.job+" "+tmpUser.id+" "+tmpUser.createdAt, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(Call<User> call, Throwable t) {
+                call.cancel();
+            }
+        });
+
+        //GET list users
+        Call<UserList> callUsers = apiInterface.doGetUserList("2");
+        callUsers.enqueue(new Callback<UserList>() {
+            @Override
+            public void onResponse(Call<UserList> call, Response<UserList> response) {
+                UserList userList = response.body();
+                Integer text = userList.page;
+                Integer total = userList.total;
+                Integer totalPages = userList.totalPages;
+                List<UserList.Datum> datumList = userList.data;
+
+                Toast.makeText(getApplicationContext(), text+" Page\n"+total+" Total\n"+totalPages+" Total Pages\n", Toast.LENGTH_SHORT).show();
+                for(UserList.Datum datum : datumList){
+                    Toast.makeText(getApplicationContext(), "id: "+datum.id+" Name: "+datum.first_name+" "+datum.last_name+" Avatar: "+ datum.avatar, Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<UserList> call, Throwable t) {
+                call.cancel();
+            }
+        });
+
+        //POST name and job URL encoded
+        Call<UserList> callUsers2 = apiInterface.doCreateUserWithField("Morpheus", "Leader");
+        callUsers2.enqueue(new Callback<UserList>() {
+            @Override
+            public void onResponse(Call<UserList> call, Response<UserList> response) {
+                UserList userList = response.body();
+                Integer text = userList.page;
+                Integer total = userList.total;
+                Integer totalPages = userList.totalPages;
+                List<UserList.Datum> datumList = userList.data;
+
+                Toast.makeText(getApplicationContext(), text+" Page\n"+total+" Total\n"+totalPages+" Total Pages\n", Toast.LENGTH_SHORT).show();
+                for(UserList.Datum datum : datumList){
+                    Toast.makeText(getApplicationContext(), "id: "+datum.id+" Name: "+datum.first_name+" "+datum.last_name+" Avatar: "+ datum.avatar, Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<UserList> call, Throwable t) {
+                call.cancel();
+            }
+        });
+
     }
 }
